b2f98021fda910ecefbec5e436dc8b394a0db860
sunsongpy <sun.song@aliyun.com>
2021-07-06 16:31:10 +0800

    内置的进制转换函数

---
+++ inbuilt/进制转换.py
@@ -0,0 +1,18 @@
+# 二进制转换为十进制
+print(int('11', base=2))
+
+# 八进制转十进制
+print(int('11', base=8))
+
+# 十六进制转十进制
+print(int('11', base=16))
+
+
+# 十进制转十六进制, 0x开头
+print(hex(17))
+
+# 十进制转八进制, 0o开头
+print(oct(9))
+
+# 十进制转二进制, 0b开头
+print(bin(3))
